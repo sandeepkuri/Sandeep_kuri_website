5a3593f4254b2dcc92dce42b9fac6a3c95d7da9f
Kuri <sandeep.kuri@cgi.com>
2017-12-15 16:25:45 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json()); //to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+    extended: true
+}));
+
+
+//Routes begin
+
+var index = require('./routes');
+var about = require('./routes/about');
+var api = require('./routes/api');
+var mailer = require('./routes/mailer');
+var logger =require('./middleware/logger');
+var jwtroute = require('./routes/jwttest');
+
+
+app.use('./', index);
+app.use('./about', about);
+app.use('./api' api);
+app.use('./mailer', mailer);
+app.use('./jwt', jwtroute);
+
+
+//route end
+
+
+// view is directory for all template files 
+
+//Error Page Routing 
+
+app.use(function(request, response, next){
+    var URL = request.protocol + '://' + request.get('host') + request.originalUrl;
+    var err = new Error('Page Not Found');
+    err.status=404;
+    logger.error('Page Not Found. URL : '+ URL+'\n'+err)
+    next();
+})
+module.exports = app;
---
+++ bin/server.js
@@ -0,0 +1,6 @@
+var app = require('../app');
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
