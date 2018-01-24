49f2828c45ac7d498ebc8c518720a86f75aa1964
Kuri <sandeep.kuri@cgi.com>
2018-01-24 12:25:53 -0400

    2412018

--- public/js/comingsoonjs.js
+++ public/js/comingsoonjs.js
@@ -1,8 +1,49 @@
-function submit(){
-    
-    var emailaddress = document.getElementById("useremail").value;
-    
 
-    
-    
-}
+
+/*-----------------------------------------------------------------------------------
+/*
+/* Comingsoon JS
+/*
+-----------------------------------------------------------------------------------*/
+
+ jQuery(document).ready(function($) {
+
+  function submit(){
+
+      $('#image-loader').fadeIn();
+
+      var emailaddress = document.getElementById("useremail").value;
+
+     
+      $.ajax({
+
+	      type: "POST",
+	      url: "http://localhost:3000/subscribe",
+	      data: {"email": emailaddress},
+	      success: function(msg) {
+
+            // Message was sent
+            if (msg == 'OK') {
+               alert("ok");
+            }
+            // There was an error
+            else {
+          alert("not ok");
+            }
+
+	      }
+
+      });
+      return false;
+   }
+
+
+});
+
+
+
+
+
+
+
+
