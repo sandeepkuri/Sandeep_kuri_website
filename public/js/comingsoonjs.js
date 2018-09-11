ae9c5e70f28ce80a1090f5f3e6f0994fc585238b
sandeep kuri <sandeepkuri002@gmail.com>
2018-09-11 07:34:47 -0300

    fixing ajax call

--- public/js/comingsoonjs.js
+++ public/js/comingsoonjs.js
@@ -6,29 +6,35 @@
 
 
 
-function submit() {
-
-    var emailaddress = document.getElementById("useremail").value;
-    var settings = {
-        "async": false,
-        "crossDomain": true,
-        "url": "http://localhost:5000/subscribe",
-        "method": "POST",
-        "headers": {
-            "content-type": "application/x-www-form-urlencoded",
-            "cache-control": "no-cache",
-            "Access-Control-Allow-Origin": '*'
-        },
-        "data": {
-            "email": emailaddress
-        }
+$(document).ready(function() {
+    window.onload = function() {
+        console.log("i'm loading");
     };
-    $.ajax(settings).done(function(response) {
-        console.log(response);
-        if (response == "send") {
-            alert("You have successfully done it ");
-        } else {
-            alert(" logged in and a Token has been issued to you");
-        }
+    $("#id_submit").click(function() {
+        var emailaddress = document.getElementById("useremail").value;
+        // alert(emailaddress);
+        console.log(emailaddress);
+        var settings = {
+            "async": false,
+            "crossDomain": true,
+            "url": "http://localhost:5000/subscribe",
+            "method": "POST",
+            "headers": {
+                "content-type": "application/x-www-form-urlencoded",
+                "cache-control": "no-cache",
+                "Access-Control-Allow-Origin": '*'
+            },
+            "data": {
+                "email": emailaddress
+            }
+        };
+        $.ajax(settings).done(function(response) {
+            console.log(response);
+            if (response == "send") {
+                alert("You have successfully done it ");
+            } else {
+                alert(" logged in and a Token has been issued to you");
+            }
+        });
     });
-}
+});
