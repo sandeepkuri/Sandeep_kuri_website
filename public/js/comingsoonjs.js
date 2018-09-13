c549c1a49ea0c116c72fd60c60c033832d312d83
sandeep kuri <sandeepkuri002@gmail.com>
2018-09-13 18:43:53 -0300

    update

--- public/js/comingsoonjs.js
+++ public/js/comingsoonjs.js
@@ -10,16 +10,22 @@ $(document).ready(function() {
     window.onload = function() {};
     $("#id_submit").click(function() {
         var emailaddress = $('#useremail').val();
+        console.log(emailaddress);
         // alert(emailaddress);
         $.ajax({
+            async: "true",
             type: "POST",
             url: "http://localhost:5000/subscribe",
             data: {
                 "email": emailaddress,
             },
             success: function(msg) {
-                console.log(msg);
 
+                if (msg == "OK") {
+                    alert("email sent");
+                } else {
+                    alert(msg);
+                }
             }
         });
 
