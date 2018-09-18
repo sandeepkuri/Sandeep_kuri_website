160229ac1c61185365758e2db307e98793de4d0b
Emmanuel Etti <etti.emman@gmail.com>
2018-09-18 18:52:03 -0300

    handling form submit event

--- public/js/comingsoonjs.js
+++ public/js/comingsoonjs.js
@@ -8,6 +8,33 @@
 
 $(document).ready(function() {
 
+    $("#contactForm").submit(function(e) {
+        debugger;
+        e.preventDefault(); // Prevents the page from refreshing
+        var $this = $(this); // `this` refers to the current form element
+        var emailaddress = document.getElementById("useremail").value;
+        $.ajax({
+
+          type: "POST",
+          url: "http://localhost:5000/subscribe",
+          data: {"email": emailaddress},
+          success: function(msg) {
+
+              // Message was sent
+              if (msg == 'OK') {
+                 alert("ok");
+              }
+              // There was an error
+              else {
+            alert("not ok");
+              }
+
+          }
+
+        });
+    });
+
+
     $("#id_submit").click(function(e) {
         e.preventDefault();
         var emailaddress = $('#useremail').val();
